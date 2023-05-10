211d0cf413424c7f780962642cafc5a5eea393c8
Mrpsousa <urameshi.uba@gmail.com>
2023-05-10 01:18:34 -0300

    feature: user and products - database

---
+++ internal/entity/product_test.go
@@ -0,0 +1,43 @@
+package entity_test
+
+import (
+	"testing"
+
+	"github.com/mrpsousa/api/internal/entity"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewProduct(t *testing.T) {
+	p, err := entity.NewProduct("Product-1", 100)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.NotEmpty(t, p.ID)
+	assert.Equal(t, "Product-1", p.Name)
+	assert.Equal(t, int64(100), p.Price)
+}
+
+func TestProductWhenNameIsRequired(t *testing.T) {
+	p, err := entity.NewProduct("", 10)
+	assert.Nil(t, p)
+	assert.Equal(t, entity.ErrNameIsRequired, err)
+
+}
+
+func TestProductWhenPriceIsRequired(t *testing.T) {
+	p, err := entity.NewProduct("Product-1", 0)
+	assert.Nil(t, p)
+	assert.Equal(t, entity.ErrPriceIsRequired, err)
+}
+
+func TestProductWhenPriceIsInvalid(t *testing.T) {
+	p, err := entity.NewProduct("Product-1", -10)
+	assert.Nil(t, p)
+	assert.Equal(t, entity.ErrInvalidPrice, err)
+}
+
+func TestProductValidate(t *testing.T) {
+	p, err := entity.NewProduct("Product-1", 10)
+	assert.Nil(t, err)
+	assert.NotNil(t, p)
+	assert.Nil(t, p.Validate())
+}
