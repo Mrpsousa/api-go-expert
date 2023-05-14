05bd0a07cb6a8b254c50fca8a3805f7e5dcb0f68
Mrpsousa <urameshi.uba@gmail.com>
2023-05-14 14:01:02 -0300

    feature: finish crud part

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,8 +1,42 @@
 package main
 
-import "github.com/mrpsousa/api/configs"
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+
+	"github.com/mrpsousa/api/configs"
+	"github.com/mrpsousa/api/internal/entity"
+	"github.com/mrpsousa/api/internal/infra/database"
+	"github.com/mrpsousa/api/internal/infra/webserver/handlers"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
 
 func main() {
-	config, _ := configs.LoadConfig("../../")
-	println(config.DBDriver)
+	_, err := configs.LoadConfig("../../")
+	if err != nil {
+		panic(err)
+	}
+
+	// println(config.DBDriver)
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+	db.AutoMigrate(&entity.Product{}, &entity.User{})
+	productDB := database.NewProduct(db)
+	productHandler := handlers.NewProductHandler(productDB)
+
+	r.Post("/products", productHandler.CreateProduct)
+	r.Get("/products/{id}", productHandler.GetProduct)
+	r.Get("/products", productHandler.GetProducts)
+	r.Put("/products/{id}", productHandler.UpdateProduct)
+	r.Delete("/products/{id}", productHandler.DeleteProduct)
+
+	http.ListenAndServe(":8000", r)
 }
