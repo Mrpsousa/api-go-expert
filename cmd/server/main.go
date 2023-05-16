4ad98b0be615ce12afa4ebe40d68bc110282a4ed
Mrpsousa <urameshi.uba@gmail.com>
2023-05-16 17:37:35 -0300

    feature:  add authentication and authorization

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/jwtauth"
 
 	"github.com/mrpsousa/api/configs"
 	"github.com/mrpsousa/api/internal/entity"
@@ -15,7 +17,7 @@ import (
 )
 
 func main() {
-	_, err := configs.LoadConfig("../../")
+	config, err := configs.LoadConfig("../../")
 	if err != nil {
 		panic(err)
 	}
@@ -26,17 +28,39 @@ func main() {
 	if err != nil {
 		panic(err)
 	}
-	r := chi.NewRouter()
-	r.Use(middleware.Logger)
+
 	db.AutoMigrate(&entity.Product{}, &entity.User{})
 	productDB := database.NewProduct(db)
 	productHandler := handlers.NewProductHandler(productDB)
+	userDB := database.NewUser(db)
+	userHandler := handlers.NewUserHandler(userDB, config.TokenAuth, config.JWTExpiresIn)
 
-	r.Post("/products", productHandler.CreateProduct)
-	r.Get("/products/{id}", productHandler.GetProduct)
-	r.Get("/products", productHandler.GetProducts)
-	r.Put("/products/{id}", productHandler.UpdateProduct)
-	r.Delete("/products/{id}", productHandler.DeleteProduct)
+	r := chi.NewRouter()
+	r.Use(middleware.Logger) // look the documentation to more diferent chi middleware
+	r.Use(middleware.Recoverer)
+	r.Use(LogRequest)
+
+	r.Route("/products", func(r chi.Router) {
+		r.Use(jwtauth.Verifier(config.TokenAuth)) // get the token and inject it into the context
+		r.Use(jwtauth.Authenticator)              // validate of token
+		r.Post("/", productHandler.CreateProduct)
+		r.Get("/{id}", productHandler.GetProduct)
+		r.Get("/", productHandler.GetProducts)
+		r.Put("/{id}", productHandler.UpdateProduct)
+		r.Delete("/{id}", productHandler.DeleteProduct)
+	})
+
+	r.Route("/users", func(r chi.Router) {
+		r.Post("/", userHandler.Create)
+		r.Post("/generate_token", userHandler.GetJWT)
+	})
 
 	http.ListenAndServe(":8000", r)
+}
+
+func LogRequest(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Println(r.Method, r.URL) //send this log to kibana
+		next.ServeHTTP(w, r)
+	})
 }
